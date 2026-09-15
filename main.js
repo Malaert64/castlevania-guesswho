76d5d75407038d7ebbe70bce0b70fad7463f18cd
TheOneCalledJ <48174721+Malaert64@users.noreply.github.com>
2026-09-15 12:45:37 -0700

    Reformatted JS a tad with more comments.

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// TODO: on left click, grey out / light up character
-// TODO: name entry page -> random character choice -> main screen
-
-/* Characters: Rinaldo Pumpkin Sara Grant Sypha Alucard Lisa Hector Julia Saint Germain Maxim Maria Tera Iris Annette Nathan Hugh Morris Cornell Reinhardt Carrie Henry Shanoa Albus Quincy John Eric Jonathan Charlotte Stella Loretta Soma Mina Yoko Hammer Arikado Kid Dracula Aeon Joachim Walter Death Mathias Dracula Isaac Carmilla Olrox Galamoth Shaft Gilles de Rais Actrise Malus Barlowe Elizabeth Brauner Graham Celia Dmitrii Dario Spear Knight Skeleton Biker Creaking Skull Talos Legion Balore Getsu Fuma Old Axe Armor Master Librarian HoD Merchant Vincent Gabriel */
+// Characters storage structure. Currently 78 in rows of 12 max, may change that.
 const CHARS = Object.freeze([
     "Leon Belmont", "Sonia Belmont", "Trevor Belmont", "Christopher Belmont", "Soleiyu Belmont", "Desmond Belmont", "Simon Belmont", "Juste Belmont", "Richter Belmont", "Julius Belmont", "Kokoro Belmont", "Sepia Belmont",
     "Grant Danasty", "Sypha Belnades", "Alucard", "Hector", "Maxim Kischine", "Maria Renard", "Shanoa", "Albus", "Nathan Graves", "Cornell", "Reinhardt Schneider", "Carrie Fernandez",
@@ -13,21 +10,24 @@ const CHARS = Object.freeze([
 ])
 
 
-/* On load, randomly select player's character and update text/img. */
+// On load, randomly select player's character and update text/img.
 window.addEventListener("load", () => {
+    // Char name for display, and reformatted to match file naming.
     let randChar = CHARS[Math.floor(Math.random() * CHARS.length)]
     let filename = "assets/Char_" + randChar.replace(/[\s.]/g, "") + ".png";
-
+    // Replace text and image via getElementById.
     document.getElementById("player-char").textContent = randChar;
     document.getElementById("player-char-img").src = filename;
 });
 
-/* Make character images clickable, and toggle between 
-colour and greyscale (or brightness, might change this...) */
+/* Make character images inside the char-imgs div clickable, 
+and toggle between colour and greyscale (or brightness, might change this...) */
 document.getElementById("char-imgs").addEventListener("click", function(event) {
   if (event.target.tagName === "IMG") {
+    // If the img has full greyscale in its filter tag, clear it. 
     if (event.target.style.filter === "grayscale(100%)") {
       event.target.style.filter = "none";
+    // Otherwise, add the greyscale.
     } else {
       event.target.style.filter = "grayscale(100%)";
     }
